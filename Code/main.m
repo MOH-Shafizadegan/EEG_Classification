fb0288e9c630a16299d364d58fa9732715a92fc1
MOH-Shafizadegan <mh.shafizadegan@gmail.com>
2024-02-02 21:27:41 +0330

    Main file updated

--- Code/main.m
+++ Code/main.m
@@ -75,7 +75,86 @@ featureScores = select_feature_fisher(Train_feature_space, TrainLabels);
 %%
 clc;
 
-n=10;
+n=30;
 selected_feature_space = ...
     select_features_using_fisherScore(Train_feature_space, featureScores, n);
 
+%% Train MLP model
+clc;
+
+n_hidden = 40;
+k = 5;
+[MLP_net, avgMSE, accuracy] = train_MLP(n_hidden, selected_feature_space, TrainLabels, k);
+
+%%
+
+clc;
+
+n_hidden = 2000;
+sigma = 0.5;
+k = 5;
+[RBF_net, RBF_avgMSE, RBF_accuracy] = train_RBF(n_hidden, sigma, selected_feature_space, TrainLabels, k);
+
+%%
+clc;
+
+n_feature = 20;
+n_comb = 1000;
+[J1_selected_features, J_featureScores] = select_feature_Jscores(Train_feature_space, n_feature, n_comb, TrainLabels);
+
+%%
+
+%% Train MLP model
+clc;
+
+n_hidden = 30;
+k = 5;
+[MLP_net_2, avgMSE_2, accuracy_2] = train_MLP(n_hidden, J1_selected_features, TrainLabels, k);
+
+
+
+%%
+clc;
+
+n_feature = 200;
+max_feature_num = 5643;
+
+% Set up PSO parameters
+options = optimoptions('particleswarm', 'SwarmSize', 50, 'MaxIterations', 100);
+
+% Define the objective function handle with extra parameters (X, k)
+objectiveFunction = @(feaure_indicies) PSO_calc_Jscore(Train_feature_space.all, feaure_indicies, TrainLabels);
+
+% Run PSO to find cluster centroids
+numVariables = n_feature;
+lb = repelem(1, n_feature);
+ub = repelem(max_feature_num, n_feature);
+
+[bestFeatureIndicies, cost] = particleswarm(objectiveFunction, numVariables, lb, ub, options);
+
+%%
+clc;
+
+n_hidden = 400;
+sigma = 0.75;
+k = 5;
+[RBF_net_2, RBF_avgMSE_2, RBF_accuracy_2] = train_RBF(n_hidden, sigma, ...
+           Train_feature_space.all(:, round(bestFeatureIndicies)), TrainLabels, k);
+
+%%
+function JScore = PSO_calc_Jscore(data, feature_Indicies, labels)
+
+    feature_Indicies = round(feature_Indicies);
+    cols = zeros(1, size(data,2));
+%     cols(feature_Indicies) = 1;
+%     disp(cols);
+    % Extract the corresponding columns from the matrix
+    selectedFeatureSpace = data(:, feature_Indicies); 
+    class1 = selectedFeatureSpace(labels == -1, :);
+    class2 = selectedFeatureSpace(labels == 1, :);
+        
+    [JScore, ~, ~] = calc_J_scores(selectedFeatureSpace, class1, class2);
+    
+    JScore = 1/JScore;
+
+end
