ef5a16685d3eef2223d663fffd7d7344afced7d8
MOH-Shafizadegan <mh.shafizadegan@gmail.com>
2024-02-03 13:37:29 +0330

    Code updated

--- Code/Functions/calc_feature_var.m
+++ Code/Functions/calc_feature_var.m
@@ -1,7 +1,6 @@
 function result = calc_feature_var(data)
 
-    % input: epoched data (channel by time by trial)
-    % output: variance feature matrix (channel by trial)
+    % result = matrix of size trials by channel
     % calculating the variance of each channel
     ch_var = var(data, 0, 2);
     result = squeeze(ch_var)';
