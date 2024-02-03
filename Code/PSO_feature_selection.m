87c3f990657e1417f2e091f6f957aa8a21e0759b
MOH-Shafizadegan <mh.shafizadegan@gmail.com>
2024-02-03 09:45:38 +0330

    Codes for finding best model

---
+++ Code/PSO_feature_selection.m
@@ -0,0 +1,52 @@
+
+clc; clear; close all;
+
+addpath('./Functions')
+
+%% Load Data in feature space
+
+Trainlabels = load('../Data/Project_data.mat').TrainLabels;
+Train_data = load('../Data/Train_feature_space.mat').Train_feature_space;
+Test_data = load('../Data/Test_feature_space.mat').Test_feature_space;
+
+%%
+clc;
+
+n_feature = 50;
+max_feature_num = 5643;
+
+% Set up PSO parameters
+options = optimoptions('particleswarm', 'SwarmSize', 50, 'MaxIterations', 100);
+
+% Define the objective function handle with extra parameters (X, k)
+objectiveFunction = @(feaure_indicies) PSO_calc_Jscore(Train_data.all, feaure_indicies, Trainlabels);
+
+% Run PSO to find cluster centroids
+numVariables = n_feature;
+lb = repelem(1, n_feature);
+ub = repelem(max_feature_num, n_feature);
+
+[bestFeatureIndicies, cost] = particleswarm(objectiveFunction, numVariables, lb, ub, options);
+
+%%
+clc;
+
+n_hidden = 1000;
+sigma = 0.5;
+k = 5;
+[RBF_net_2, RBF_avgMSE_2, RBF_accuracy_2] = train_RBF(n_hidden, sigma, ...
+           Train_data.all(:, round(bestFeatureIndicies)), Trainlabels, k);
+
+%%
+function out = PSO_calc_Jscore(data, feature_Indicies, labels)
+
+    feature_Indicies = round(feature_Indicies);
+    selectedFeatureSpace = data(:, feature_Indicies); 
+    class1 = selectedFeatureSpace(labels == -1, :);
+    class2 = selectedFeatureSpace(labels == 1, :);
+        
+    [JScore, ~, ~] = calc_J_scores(selectedFeatureSpace, class1, class2);
+    
+    out = 1/JScore;
+
+end
